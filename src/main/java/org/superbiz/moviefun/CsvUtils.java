9382a196c7d991ec04331643c79ab9ec31bf28a2
Savith Kumar <savithk@gmail.com>
2020-05-13 17:03:59 -0700

    movie fun - Changes made for "Removing Reads from File System" lab exercise.

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,18 +14,23 @@ import java.util.Scanner;
 public class CsvUtils {
 
     public static String readFile(String path) {
-        try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
 
-            if (scanner.hasNext()) {
-                return scanner.next();
-            } else {
-                return "";
-            }
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        if(path == null) {
+            throw new IllegalArgumentException("Path cannot be null");
         }
+
+        ClassLoader classLoader = CsvUtils.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(path);
+        assert inputStream != null;
+
+        Scanner scanner = new Scanner(inputStream).useDelimiter("\\A");
+
+        if (scanner.hasNext()) {
+            return scanner.next();
+        } else {
+            return "";
+        }
+
     }
 
     public static <T> List<T> readFromCsv(ObjectReader objectReader, String path) {
